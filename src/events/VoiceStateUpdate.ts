b019dbfd5527ed7fe365a06c7d6bf4951fcf0371
Ivan <ilitteri@fi.uba.ar>
2021-09-04 00:32:18 -0300

    Refactor Renamed  & assigned variable names

--- src/events/VoiceStateUpdate.ts
+++ src/events/VoiceStateUpdate.ts
@@ -7,8 +7,11 @@ export const execute: ExecuteFunction = async (
     oldState: VoiceState,
     newState: VoiceState
 ) => {
+    const oldChannel = oldState.channel;
+    const newChannel = newState.channel;
     const member: GuildMember = newState.member!;
-    if (oldState.channelId) {
+
+    if (oldChannel?.id) {
         if (
             oldState.channel!.parentId === config.mitosisCategoryID &&
             oldState.channelId != config.mitosisVoiceChannelID &&
@@ -18,15 +21,15 @@ export const execute: ExecuteFunction = async (
         }
     }
 
-    if (newState.channelId === config.mitosisVoiceChannelID) {
-        const newChannel = await newState.channel!.guild!.channels.create(
-            'Upa, no pudiste',
+    if (newChannel!.id === config.mitosisVoiceChannelID) {
+        const createdChannel = await newState.channel!.guild!.channels.create(
+            'Hijo',
             {
                 type: 'GUILD_VOICE',
                 parent: newState.channel!.parent!,
             }
         );
-        member.voice.setChannel(newChannel);
+        member.voice.setChannel(createdChannel);
     }
 };
 
