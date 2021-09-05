435b63945f7c3699e0993192a50a1bdb2ee2c6ce
Ivan <ilitteri@fi.uba.ar>
2021-09-05 15:30:57 -0300

    Add: no messages can be sent in query queue text channels

--- src/events/MessageCreateEvent.ts
+++ src/events/MessageCreateEvent.ts
@@ -3,19 +3,29 @@ import { ExecuteFunction } from '../interfaces/Event';
 import { client } from '../index';
 const wait = require('util').promisify(setTimeout);
 
+async function deleteMessageAndReply(message: Message, response: string) {
+    await message.delete();
+    const reply = await message.channel.send({ content: response });
+    await wait(5000);
+    await reply.delete();
+}
+
 export const execute: ExecuteFunction = async (message: Message) => {
     if (
         client.config.onlyThreadsTextChannelsIDs.includes(message.channelId) &&
         message.type !== 'THREAD_CREATED' &&
         message.member!.id !== client.config.clientID
     ) {
-        await message.delete();
-        const reply = await message.channel.send({
-            content:
-                'Este canal es sólo para hilos! Antes de crear uno, revisá que el tema no se esté tratando en otro hilo',
-        });
-        await wait(5000);
-        await reply.delete();
+        deleteMessageAndReply(
+            message,
+            'Este canal es sólo para hilos! Antes de crear uno, revisá que el tema no se esté tratando en otro hilo'
+        );
+    } else if (
+        message.author.id !== client.config.clientID &&
+        (message.channelId === client.config.studentsQueryChannelID ||
+            message.channelId === client.config.teachersQueryChannelID)
+    ) {
+        deleteMessageAndReply(message, 'No se puede escribir en este canal!');
     }
 };
 
