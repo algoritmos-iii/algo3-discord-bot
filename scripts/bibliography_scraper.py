1f3b2e4e525ffdf3ee3dfb36766edc7aceaa78f7
Ivan <ilitteri@fi.uba.ar>
2021-09-04 22:33:40 -0300

    Add bibliography scraper Web scraping python script that scraps the papers links

---
+++ scripts/bibliography_scraper.py
@@ -0,0 +1,50 @@
+from typing import Pattern
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import json
+
+
+def write_json(on: str, data: dict):
+    with open(on, "w") as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+def rebuild_links(concrete_links_peaces: list) -> list:
+    return list(map(lambda peaces: f"http://{peaces[2]}/{'/'.join(peaces[5:])}", concrete_links_peaces))
+
+
+def split_link(hrefs: list) -> list:
+    return list(map(lambda pseudo_link: pseudo_link.split("/"), hrefs))
+
+
+def filter_papers_links(hrefs: list) -> list:
+    concrete_links_peaces = split_link(hrefs)
+    concrete_links = rebuild_links(concrete_links_peaces)
+    return concrete_links, concrete_links_peaces
+
+
+def find_all_a_tags_that_matches_pattern(soup, pattern: Pattern[str]):
+    return soup.findAll('a', attrs={'href': pattern})
+
+
+def parse_html(html_page):
+    return BeautifulSoup(html_page, "html.parser")
+
+
+def get_html_from(url: str):
+    return urllib.request.urlopen(url)
+
+
+def main():
+    html_page = get_html_from("https://github.com/algoritmos-iii/algoritmos-iii.github.io/tree/master/assets/bibliografia")
+    soup = parse_html(html_page)
+    pattern = re.compile("^/algoritmos-iii/algoritmos-iii.github.io/blob/master/assets/bibliografia/")
+    a_tags = find_all_a_tags_that_matches_pattern(soup, pattern)
+    hrefs = [link.get('href') for link in a_tags]
+    papers_links, links_peaces = filter_papers_links(hrefs)
+    name_link_dict = {links_peaces[i][-1].rstrip(".pdf"):papers_links[i] for i in range(len(papers_links))}
+    write_json(on="../assets/bibliography_links.json", data=name_link_dict)
+
+
+main()
