99938776d8b78e657ceb6fcf1b3ddd03a19bc927
Ivan <ilitteri@fi.uba.ar>
2021-09-04 22:36:32 -0300

    Update Now there can only be one member of the same group queued

--- src/buttons/QueueQueryButton.ts
+++ src/buttons/QueueQueryButton.ts
@@ -3,12 +3,27 @@ import { client } from '../index';
 
 export const execute = async (interaction: ButtonInteraction) => {
     const member = interaction.member as GuildMember;
+    const group = member.roles.cache.find((role) =>
+        role.name.startsWith('Grupo')
+    );
     if (client.queryQueue.indexOf(member) != -1) {
         await interaction.reply({
             content: 'Ya estás en la cola de espera!',
             ephemeral: true,
         });
         return;
+    }
+    if (group) {
+        const relatives = client.queryQueue.filter((member) =>
+            member.roles.cache.some((role) => role.name === group.name)
+        );
+        if (relatives.length > 0) {
+            await interaction.reply({
+                content: 'Ya hay alguien de tu grupo en la cola de espera!',
+                ephemeral: true,
+            });
+            return;
+        }
     }
     client.logger.info(interaction.user.tag, 'quiere ayuda');
     client.queryQueue.push(member);
