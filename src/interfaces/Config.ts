0bd72b346457b3116632e752f37102d7146b771e
Ivan <ilitteri@fi.uba.ar>
2021-09-05 23:58:56 -0300

    AddL config properties - Teacher role id - Student role id

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -1,10 +1,11 @@
 export interface Config {
     clientID: string;
     token: string;
+    teacherRoleID: string;
+    studentRoleID: string;
     mitosisVoiceChannelID: string;
     mitosisCategoryID: string;
     studentsQueryChannelID: string;
     teachersQueryChannelID: string;
-    createdChannelsFilePath: string;
     onlyThreadsTextChannelsIDs: string[];
 }
