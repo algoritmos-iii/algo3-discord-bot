3a7cf6728a8af7d80e6e803d9e1578a77f66751d
Ivan <ilitteri@fi.uba.ar>
2021-09-06 00:01:10 -0300

    Fix: now role is finded by id & the client logger is used

--- src/events/GuildMemberAddEvent.ts
+++ src/events/GuildMemberAddEvent.ts
@@ -1,18 +1,18 @@
-import consola from 'consola';
 import { GuildMember } from 'discord.js';
 import { ExecuteFunction } from '../interfaces/Event';
+import { client } from '../index';
 
 export const execute: ExecuteFunction = async (newMember: GuildMember) => {
-    console.log(
+    client.logger.log(
         `${newMember.user.username} has joined ${newMember.guild.name}`
     );
     await newMember.roles
         .add(
             newMember.guild.roles.cache.find(
-                (role) => role.name === 'newcomer'
+                (role) => role.id === client.config.studentRoleID
             )!
         )
-        .catch(consola.error);
+        .catch(client.logger.error);
 };
 
 export const name: string = 'guildMemberAdd';
