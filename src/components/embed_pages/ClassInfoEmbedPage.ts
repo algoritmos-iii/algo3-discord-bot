5563fad2318a8870445dd453b1943b9f958f036f
Ivan <ilitteri@fi.uba.ar>
2021-09-27 22:02:13 -0300

    Fix: class details time zone

--- src/components/embed_pages/ClassInfoEmbedPage.ts
+++ src/components/embed_pages/ClassInfoEmbedPage.ts
@@ -22,8 +22,12 @@ export const data = new EmbedPage(
         {
             name: 'Detalles de la clase',
             value: `Empieza el ${dateFromISO(
-                event.start.dateTime
-            )} y termina el ${dateFromISO(event.end.dateTime)}`,
+                event.start.dateTime,
+                event.start.timeZone
+            )} y termina el ${dateFromISO(
+                event.end.dateTime,
+                event.end.timeZone
+            )}`,
         },
         {
             name: 'Temas',
@@ -61,7 +65,7 @@ export const data = new EmbedPage(
         .roles.cache.get(client.config.studentRoleID)}`
 );
 
-function dateFromISO(isoDate: string) {
+function dateFromISO(isoDate: string, timeZone: string) {
     const date = new Date(isoDate);
     return date.toLocaleTimeString('es-ES', {
         weekday: 'long',
@@ -71,5 +75,6 @@ function dateFromISO(isoDate: string) {
         hour: 'numeric',
         minute: 'numeric',
         hour12: false,
+        timeZone: timeZone,
     });
 }
