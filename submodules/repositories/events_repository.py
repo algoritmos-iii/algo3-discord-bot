eb9aa902ec919e70acd0cf2f428c862818dda8d6
Iván Litteri <ilitteri@fi.uba.ar>
2022-04-14 17:59:42 -0300

    Fix response

--- submodules/repositories/events_repository.py
+++ submodules/repositories/events_repository.py
@@ -13,15 +13,27 @@ class EventsRepository:
         return self.__service.service().events()
 
     def next_event(self) -> Dict[str, Any]:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        # now = datetime.datetime.utcnow().isoformat() + 'Z'
+        now = datetime.datetime.utcnow()
         next_class = now + datetime.timedelta(days = 4)
-        print(f'Getting event from: {now.split()}')
-        request = self.events().list(calendarId=self.__calendar_id, timeMin=now, timeMax = next_class, 
+        print(f'Getting event from: {now.isoformat().split()}')
+        request = self.events().list(calendarId=self.__calendar_id, timeMin=now.isoformat()+"Z", timeMax = next_class.isoformat()+"Z", 
                                      maxResults=1, singleEvents=True, orderBy='startTime')
         response = request.execute()
 
         if len(response.get('items', [])) == 0:
             print('No upcoming events found. Probably is holiday')
-            return {"summary": "Holiday"}
+            return {
+                "summary": "Holiday",
+                "eventDescription": "NO HAY CLASES",
+                "start": {
+                    "dateTime": "",
+                    "timeZone": ""
+                },
+                "end": {
+                    "dateTime": "",
+                    "timeZone": ""
+                },
+            }
 
         return response.get('items', [])[0]
